ab6f9292cd752a4b3cbe18a12e0606ca6082b88c
Chris Lu <chrislu0126@hotmail.com>
2017-11-24 14:50:39 -0500

    icmp might work bugs fix

--- router/sr_router.c
+++ router/sr_router.c
@@ -237,7 +237,7 @@ int sr_nat_handleIPpacket(struct sr_instance* sr,
                     ip_packet->ip_ttl --;
                     ip_packet->ip_sum = 0;
                     ip_packet->ip_sum = cksum((uint8_t *) ip_packet, sizeof(sr_ip_hdr_t));
-                    icmp_hdr->icmp_sum = cksum(icmp_hdr, 64);
+                    icmp_hdr->icmp_sum = cksum(icmp_hdr, len - sizeof(sr_ethernet_hdr_t) - sizeof(sr_ip_hdr_t));
                     printf("Found entry in routing table.\n");
                     /* Check ARP cache, see hit or miss, like can we find the MAC addr.. */
                     struct sr_arpcache *cache = &(sr->cache);
@@ -365,7 +365,7 @@ int sr_nat_handleIPpacket(struct sr_instance* sr,
                 ip_packet->ip_ttl --;
                 ip_packet->ip_sum = 0;
                 ip_packet->ip_sum = cksum((uint8_t *) ip_packet, sizeof(sr_ip_hdr_t));
-                icmp_hdr->icmp_sum = cksum(icmp_hdr, 64);
+                icmp_hdr->icmp_sum = cksum(icmp_hdr, len - sizeof(sr_ethernet_hdr_t) - sizeof(sr_ip_hdr_t));
                 
                 printf("Found entry in routing table.\n");
                 /* Check ARP cache, see hit or miss, like can we find the MAC addr.. */
